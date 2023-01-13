a39e2099654d6bacfd19890059886eafbfa43a55
Denis <den_makarcev@mail.ru>
2023-01-13 10:58:06 +0300

    doc

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+Console.Clear(); // очистка консоли
 Console.WriteLine("Hello, World!");
+*/
+// ctrl + /
+// Console.Clear();
+// int a = 1, b = 2;
+// Console.WriteLine($"{a}, {b}");
+// int n = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Результат: {n * 3}");
+// Результат: 15
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n > m)
+//     Console.WriteLine(n);
+// else if (n < m)
+//     Console.WriteLine(m);
+// else
+//     Console.WriteLine("=");
+
